23e76c0c559c9c61c6dbdfc3589732b03d9913c5
Vaibhav Mantri <52986202+vaibhavmantri1@users.noreply.github.com>
2020-01-26 16:05:05 -0800

    update

---
+++ js/main.js
@@ -0,0 +1,3 @@
+function look(){
+document.getElementById("google_map").src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7144.103719526606!2d80.2710461727338!3d26.4540572387751!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399c47e4d0fc860d%3A0xfad307fab3a2afef!2sAzad%20Nagar%2C%20Kanpur%2C%20Uttar%20Pradesh%20208022!5e0!3m2!1sen!2sin!4v1580083035805!5m2!1sen!2sin";
+}
