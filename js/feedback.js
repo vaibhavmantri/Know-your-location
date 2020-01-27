cb18670e2f1ace3ef4b3e991d82cb548caa71f05
Vaibhav Mantri <52986202+vaibhavmantri1@users.noreply.github.com>
2020-01-28 01:03:55 +0530

    updated

---
+++ js/feedback.js
@@ -0,0 +1,12 @@
+var name1 = document.getElementById("InputName1");
+var name2 = document.getElementById("InputName2");
+var email = document.getElementById("InputEmail1");
+var area = document.getElementById("area").value;
+var feedback = document.getElementById("FormControltextarea1");
+function feedbacks(){
+console.log(name1);
+console.log(name2);
+console.log(email);
+console.log(area);
+console.log(feedback);
+}
