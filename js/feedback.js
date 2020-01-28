84a1301b00998645f62ae98f7beeebc671674963
Vaibhav Mantri <52986202+vaibhavmantri1@users.noreply.github.com>
2020-01-28 23:43:53 +0530

    update

--- js/feedback.js
+++ js/feedback.js
@@ -1,12 +1,17 @@
-var name1 = document.getElementById("InputName1");
-var name2 = document.getElementById("InputName2");
-var email = document.getElementById("InputEmail1");
-var area = document.getElementById("area").value;
-var feedback = document.getElementById("FormControltextarea1");
-function feedbacks(){
-console.log(name1);
-console.log(name2);
-console.log(email);
-console.log(area);
-console.log(feedback);
+var name1;
+var name2;
+var email;
+var area;
+var feedback;
+function feedbacks() {
+    name1 = document.getElementById("InputName1").value;
+    name2 = document.getElementById("InputName2").value;
+    email = document.getElementById("InputEmail1").value;
+    area = document.getElementById("area").value;
+    feedback = document.getElementById("FormControlTextarea1").value;
+    console.log(name1);
+    console.log(name2);
+    console.log(email);
+    console.log(area);
+    console.log(feedback);
 }
